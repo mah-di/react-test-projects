1fbcd5c571d3e65fd05807023dbb7235e38deeba
Iqbal Mahdi <mahdiiqbal37@gmail.com>
2023-04-02 20:09:01 +0600

    filter values from queryparams are now assigned to relevant fields on page load and some slight refactoring

--- src/CountryApp/components/App.js
+++ src/CountryApp/components/App.js
@@ -30,29 +30,37 @@ const initialState = {
 }
 
 const App = () => {
+    const navigate = useNavigate()
     const [ filterSet, setFilterSet ] = useState(null)
     const [ filters, setFilters ] = useSearchParams()
-    const navigate = useNavigate()
     const { data, isLoading, error } = useFetchCountries('https://restcountries.com/v3.1/all')
     const [ displayData, dispatch ] = useReducer(reducer, initialState)
     const { filteredData, sortByName, sortByPopulation, sortByArea, totalCountries } = displayData
+
+    const region = filters.get('region')
+    const minPopulation = filters.get('population_min')
+    const maxPopulation = filters.get('population_max')
+    const minArea = filters.get('area_min')
+    const maxArea = filters.get('area_max')
 
     useEffect(() => {
         data && dispatch({type: 'LOAD', payload: data})
     }, [data])
 
     useEffect(() => {
-        data && filterSet && setFilters({...filterSet})
-    }, [filterSet, setFilters, data])
+        if ( data ) {
+            region && setFilterSet((prev) => {return {...prev, region}})
+            minPopulation && setFilterSet((prev) => {return {...prev, population_min: minPopulation}})
+            maxPopulation && setFilterSet((prev) => {return {...prev, population_max: maxPopulation}})
+            minArea && setFilterSet((prev) => {return {...prev, area_min: minArea}})
+            maxArea && setFilterSet((prev) => {return {...prev, area_max: maxArea}})
+            dispatch({type: 'FILTER', payload: {region, minPopulation, maxPopulation, minArea, maxArea, data}})
+        }
+    }, [data, region, minPopulation, maxPopulation, minArea, maxArea])
 
     useEffect(() => {
-        const region = filters.get('region')
-        const minPopulation = filters.get('population_min')
-        const maxPopulation = filters.get('population_max')
-        const minArea = filters.get('area_min')
-        const maxArea = filters.get('area_max')
-        data && dispatch({type: 'FILTER', payload: {region, minPopulation, maxPopulation, minArea, maxArea, data}})
-    }, [filters, data])
+        data && filterSet && setFilters({...filterSet})
+    }, [filterSet, setFilters, data])
 
     const handleRemove = (id) => {
         dispatch({type: 'REMOVE', payload: id})
@@ -74,35 +82,43 @@ const App = () => {
     }
 
     const handleAreaFilter = (area) => {
-        if ( area.minArea !== '' ) {
+        area.minArea !== ''
+            ?
             setFilterSet((prev) => {return {...prev, area_min: area.minArea}})
-        }
-        if ( area.maxArea !== '' ) {
-            setFilterSet((prev) => {return {...prev, area_max: area.maxArea}})
-        }
-        if ( area.minArea === '' && area.maxArea === '' ) {
+            :
             setFilterSet((prev) => {
                 delete prev.area_min
+                return {...prev}
+            })
+
+        area.maxArea !== ''
+            ?
+            setFilterSet((prev) => {return {...prev, area_max: area.maxArea}})
+            :
+            setFilterSet((prev) => {
                 delete prev.area_max
                 return {...prev}
             })
-        }
     }
 
     const handlePopulationFilter = (population) => {
-        if ( population.minPopulation !== '' ) {
+        population.minPopulation !== ''
+            ?
             setFilterSet((prev) => {return {...prev, population_min: population.minPopulation}})
-        }
-        if ( population.maxPopulation !== '' ) {
-            setFilterSet((prev) => {return {...prev, population_max: population.maxPopulation}})
-        }
-        if ( population.minPopulation === '' && population.maxPopulation === '' ) {
+            :
             setFilterSet((prev) => {
                 delete prev.population_min
+                return {...prev}
+            })
+        
+        population.maxPopulation !== ''
+            ?
+            setFilterSet((prev) => {return {...prev, population_max: population.maxPopulation}})
+            :
+            setFilterSet((prev) => {
                 delete prev.population_max
                 return {...prev}
             })
-        }
     }
 
     const handleNameSort = () => {
@@ -122,7 +138,7 @@ const App = () => {
             {isLoading && <h4 className={style['total-countries-count']}>Loading, please wait...</h4>}
             {error && <h4 className={style['total-countries-count']}>{error}</h4>}
             {!isLoading && error === null && filteredData && 
-                <FiltersContext.Provider value={{onRegionFilter: handleRegionFilter, onAreaFilter: handleAreaFilter, onPopulationFilter: handlePopulationFilter}} >
+                <FiltersContext.Provider value={{onRegionFilter: handleRegionFilter, onAreaFilter: handleAreaFilter, onPopulationFilter: handlePopulationFilter, filterSet}} >
                     <Filters />
                 </FiltersContext.Provider>
             }
--- src/CountryApp/components/Filters.js
+++ src/CountryApp/components/Filters.js
@@ -1,44 +1,40 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext, useEffect } from 'react'
 import { FiltersContext } from '../contexts/FiltersContext'
 
 import style from '../css/Filter.module.css'
 
-const initialFilterState = {
-    Asia: false,
-    Africa: false,
-    Americas: false,
-    Antarctic: false,
-    Europe: false,
-    Oceania: false,
-}
-
 const FilterByRegion = () => {
-    const { onRegionFilter } = useContext(FiltersContext)
-    const [ filterState, setFilteState ] = useState(initialFilterState)
+    const { onRegionFilter, filterSet } = useContext(FiltersContext)
+    const { region } = filterSet
 
     const handleClick = (e) => {
-        const region = e.target.value
-        setFilteState({...initialFilterState, [region]: true})
-        onRegionFilter(region)
+        const setRegion = e.target.value
+        onRegionFilter(setRegion)
     }
 
     return (
         <div className={style['filter-wrapper']}>
             <button className={style.btn} value='' onClick={handleClick}>All</button>
-            <button className={`${style.btn} ${filterState.Asia && style['btn-active']}`} value='Asia' onClick={handleClick}>Asia</button>
-            <button className={`${style.btn} ${filterState.Africa && style['btn-active']}`} value='Africa' onClick={handleClick}>Africa</button>
-            <button className={`${style.btn} ${filterState.Americas && style['btn-active']}`} value='Americas' onClick={handleClick}>Americas</button>
-            <button className={`${style.btn} ${filterState.Antarctic && style['btn-active']}`} value='Antarctic' onClick={handleClick}>Antarctic</button>
-            <button className={`${style.btn} ${filterState.Europe && style['btn-active']}`} value='Europe' onClick={handleClick}>Europe</button>
-            <button className={`${style.btn} ${filterState.Oceania && style['btn-active']}`} value='Oceania' onClick={handleClick}>Oceania</button>
+            <button className={`${style.btn} ${region === 'Asia' && style['btn-active']}`} value='Asia' onClick={handleClick}>Asia</button>
+            <button className={`${style.btn} ${region === 'Africa' && style['btn-active']}`} value='Africa' onClick={handleClick}>Africa</button>
+            <button className={`${style.btn} ${region === 'Americas' && style['btn-active']}`} value='Americas' onClick={handleClick}>Americas</button>
+            <button className={`${style.btn} ${region === 'Antarctic' && style['btn-active']}`} value='Antarctic' onClick={handleClick}>Antarctic</button>
+            <button className={`${style.btn} ${region === 'Europe' && style['btn-active']}`} value='Europe' onClick={handleClick}>Europe</button>
+            <button className={`${style.btn} ${region === 'Oceania' && style['btn-active']}`} value='Oceania' onClick={handleClick}>Oceania</button>
         </div>
-  )
+    )
 }
 
 const FilterByPopulation = () => {
-    const { onPopulationFilter } = useContext(FiltersContext)
+    const { onPopulationFilter, filterSet } = useContext(FiltersContext)
+    const { population_min, population_max } = filterSet
     const [ population, setPopulation ] = useState({minPopulation: '', maxPopulation: ''})
 
+    useEffect(() => {
+        population_min && setPopulation((prev) => {return {...prev, minPopulation: population_min}})
+        population_max && setPopulation((prev) => {return {...prev, maxPopulation: population_max}})
+    }, [population_min, population_max])
+    
     const handleChange = (e) => {
         const name = e.target.name
         setPopulation(prev => {return {...prev, [name]: e.target.value}})
@@ -59,8 +55,14 @@ const FilterByPopulation = () => {
 }
 
 const FilterByArea = () => {
-    const { onAreaFilter } = useContext(FiltersContext)
+    const { onAreaFilter, filterSet } = useContext(FiltersContext)
+    const { area_min, area_max } = filterSet
     const [ area, setArea ] = useState({minArea: '', maxArea: ''})
+
+    useEffect(() => {
+        area_min && setArea((prev) => {return {...prev, minArea: area_min}})
+        area_max && setArea((prev) => {return {...prev, minArea: area_max}})
+    }, [area_min, area_max])
 
     const handleChange = (e) => {
         const name = e.target.name
@@ -82,13 +84,13 @@ const FilterByArea = () => {
 }
 
 const Filters = () => {
-  return (
-    <div>
-        <FilterByPopulation />
-        <FilterByArea />
-        <FilterByRegion />
-    </div>
-  )
+    return (
+        <div>
+            <FilterByPopulation />
+            <FilterByArea />
+            <FilterByRegion />
+        </div>
+    )
 }
 
 export default Filters
